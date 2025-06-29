c4fd385f5b377aa9f1209da9ba260a64a39c6416
Adriano de Oliveira <5173823+odrianoaliveira@users.noreply.github.com>
2025-06-29 11:02:43 +0200

    race condition scenarion

---
+++ src/main/java/com/adriano/threading/racecondition/RaceCondition.java
@@ -0,0 +1,21 @@
+
+public class RaceCondition {
+  private static volatile boolean done; //without volatile the variable get cached or stored in register without cross the memory barrier
+  //that means others thread can't see the modification
+
+  public static void main(String [] args) throws Exception {
+    new Thread(
+      new Runnable() {
+        public void run() {
+          int i=0;
+          while(!done) { i++; }
+          System.out.println("Done!");
+        }
+      }).start();
+
+  System.out.println("OS: " + System.getProperty("os.name"));
+  Thread.sleep(2000);
+  done = true;
+  System.out.println("flag done set to true");
+}
+}
